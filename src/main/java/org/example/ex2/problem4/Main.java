8e78c4ac86b9c643a1a99e9397374d954cf2168b
JannisLiebscher <jannis-liebscher@gmx.de>
2025-03-29 13:07:31 +0100

    EX 2.4 Zeitmessung

--- src/main/java/org/example/ex2/problem4/Main.java
+++ src/main/java/org/example/ex2/problem4/Main.java
@@ -9,16 +9,18 @@ import java.security.MessageDigest;
 public class Main {
     static BigInteger winnerNonce = new BigInteger("-1");
     static BigInteger winnerHash;
+    static long winnerTime;
 
     public static void main(String[] args) throws InterruptedException {
 
         ExecutorService executor = Executors.newFixedThreadPool(20);
         BigInteger nonce = new BigInteger("0");
+        long start = System.currentTimeMillis();
         while (winnerNonce.equals(new BigInteger("-1"))) {
             BigInteger finalNonce = nonce;
             executor.submit(() -> {
                 try {
-                    service("new block", new BigInteger("400000000000000000000000000000000000000000000000000000000000000000000000"), finalNonce);
+                    service("new block", new BigInteger("40000000000000000000000000000000000000000000000000000000000000000000000"), finalNonce);
                 } catch (NoSuchAlgorithmException e) {
                     throw new RuntimeException(e);
                 }
@@ -26,12 +28,15 @@ public class Main {
             nonce = nonce.add(new BigInteger("1"));
 
         }
+        long end = System.currentTimeMillis();
+        winnerTime = (end - start) / 1000;
         executor.shutdown();
         while (!executor.isTerminated()) {
             Thread.sleep(100);
         }
         System.out.println("Nonce found: " + winnerNonce);
         System.out.println("Hash: " + winnerHash);
+        System.out.println("Time spend: " + winnerTime + " S");
         System.out.println("DONE");
     }
 
