8d207facf0b7e880107e4701b5abdb212fabd1ad
JannisLiebscher <jannis-liebscher@gmx.de>
2025-04-18 18:05:20 +0200

    ex4

--- src/main/rust/ex4/src/main.rs
+++ src/main/rust/ex4/src/main.rs
@@ -1,3 +1,23 @@
+const ARRAY_SIZE: usize = 10;
 fn main() {
-    println!("Hello, world!");
+    let number: u32 = 150;
+    let factors:[u32; ARRAY_SIZE];
+    factors = factorizer(number);
+    for element in factors {
+        print!("{element}, ");
+    }
 }
+
+fn factorizer(mut number: u32) -> [u32; ARRAY_SIZE] {
+    let mut counter = 0;
+    let mut factors = [0; ARRAY_SIZE];
+    for i in 2..number + 1 {
+        while number % i == 0 {
+            factors[counter] = i;
+            number /=i;
+            counter += 1;
+        }
+    }
+    factors
+}
+fn print_result(number: u32) {}
---
+++ src/main/rust/ex4/target/debug/deps/ex4-90971693a8aca7cb.d
@@ -0,0 +1,5 @@
+C:\Users\Jannis\IdeaProjects\Concurrency\src\main\rust\ex4\target\debug\deps\libex4-90971693a8aca7cb.rmeta: src\main.rs
+
+C:\Users\Jannis\IdeaProjects\Concurrency\src\main\rust\ex4\target\debug\deps\ex4-90971693a8aca7cb.d: src\main.rs
+
+src\main.rs:
